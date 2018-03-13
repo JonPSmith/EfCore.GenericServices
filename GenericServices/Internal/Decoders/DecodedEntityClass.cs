baa3208f4cd31966b95a79ccf1be1f89adb44d7d
Jon Smith <jon.smith@selectiveanalytics.com>
2018-03-13 20:14:55 +0000

    Setup DecodedEntityClass

--- GenericServices/Internal/Decoders/DecodedEntityClass.cs
+++ GenericServices/Internal/Decoders/DecodedEntityClass.cs
@@ -2,42 +2,47 @@
 // Licensed under MIT licence. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using GenericLibsBase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Remotion.Linq.Parsing;
 
 namespace GenericServices.Internal.Decoders
 {
     internal class DecodedEntityClass
     {
-        
 
         public Type EntityType { get; private set; }
 
         public ImmutableList<IKey> PrimaryKeys { get; private set; }
 
-        public bool CanBeUpdatedByProperties { get; private set; }
+        public DecodedClass EntityClassInfo { get; private set; }
 
-        static IStatusGeneric<T> CreateFactory<T>( Type entityType, DbContext context) where T : class
+        private DecodedEntityClass(Type entityType, IEnumerable<IKey> primaryKeys, DecodedClass entityClassInfo)
         {
-            var status = new StatusGenericHandler();
+            EntityType = entityType ?? throw new ArgumentNullException(nameof(entityType));
+            PrimaryKeys = primaryKeys?.ToImmutableList() ?? throw new ArgumentNullException(nameof(primaryKeys));
+            EntityClassInfo = entityClassInfo ?? throw new ArgumentNullException(nameof(entityClassInfo));
+        }
+
+        static IStatusGeneric<DecodedEntityClass> CreateFactory( Type entityType, DbContext context)
+        {
+            var status = new StatusGenericHandler<DecodedEntityClass>();
             var efType = context.Model.FindEntityType(entityType.FullName);
             if (efType == null)
             {
                 status.AddError($"The class {entityType.Name} was not found in the {context.GetType().Name} DbContext.");
-                return null;
+                return status;
             }
 
-            var publicCtors = entityType.GetConstructors();
-            var allPublicMethod = entityType.GetMethods();
-            var publicStaticMethods = allPublicMethod.Where(x => x.IsStatic);
-            var publicMethods = allPublicMethod.Where(x => x.IsStatic);
-            var propertiesWithPublicSetter = entityType.GetProperties();
-
-            throw new NotImplementedException();
+            status.Result = new DecodedEntityClass(entityType, 
+                efType.GetKeys().Where(x => x.IsPrimaryKey()),
+                new DecodedClass(entityType));
+            return status;
         }
     }
 }
