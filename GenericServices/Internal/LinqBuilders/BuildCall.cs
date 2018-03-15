a1a1793cc52e3b7e48bebc713e28e835d1b5276d
Jon Smith <jon.smith@selectiveanalytics.com>
2018-03-15 15:13:01 +0000

    Got method call vai LINQ working

--- GenericServices/Internal/LinqBuilders/BuildCall.cs
+++ GenericServices/Internal/LinqBuilders/BuildCall.cs
@@ -5,22 +5,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using GenericLibsBase;
+
+[assembly: InternalsVisibleTo("Tests")]
 
 namespace GenericServices.Internal.LinqBuilders
 {
-    internal class BuildCall
+    internal static class BuildCall
     {
-        public Action<object,object> CallMethodReturnVoid(Type tIn, Type tClass, MethodInfo methodInfo, params PropertyInfo[] properties)
+        public static Action<TIn, TClass> CallMethodReturnVoid<TIn, TClass>(this MethodInfo methodInfo, params PropertyInfo[] properties)
         {
-            var pIn = Expression.Parameter(tIn, "x");
-            var pCall = Expression.Parameter(tClass, "y");
+            var pIn = Expression.Parameter(typeof(TIn), "x");
+            var pCall = Expression.Parameter(typeof(TClass), "y");
             var args = new List<MemberExpression>();
             foreach (var propertyInfo in properties)
             {
                 args.Add( Expression.Property(pIn, propertyInfo));
             }
             var call = Expression.Call(pCall, methodInfo, args);
-            var built = Expression.Lambda<Action<object,object>>(call, false, new[] { pIn, pCall });
+            var built = Expression.Lambda<Action<TIn, TClass>>(call, false, new[] { pIn, pCall });
+            return built.Compile();
+        }
+
+        public static Func<TIn, TClass, IStatusGeneric> CallMethodReturnStatus<TIn, TClass>(this MethodInfo methodInfo, params PropertyInfo[] properties)
+        {
+            var pIn = Expression.Parameter(typeof(TIn), "x");
+            var pCall = Expression.Parameter(typeof(TClass), "y");
+            var args = new List<MemberExpression>();
+            foreach (var propertyInfo in properties)
+            {
+                args.Add(Expression.Property(pIn, propertyInfo));
+            }
+            var call = Expression.Call(pCall, methodInfo, args);
+            var built = Expression.Lambda<Func<TIn, TClass, IStatusGeneric>>(call, false, new[] { pIn, pCall });
             return built.Compile();
         }
     }
---
+++ Tests/UnitTests/GenericServicesInternal/TestBuildCall.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using GenericLibsBase;
+using GenericServices.Internal.LinqBuilders;
+using Xunit;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Tests.UnitTests.GenericServicesInternal
+{
+    public class TestBuildCall
+    {
+
+        private class Dto
+        {
+            public int MyInt { get; set; }
+            public string MyString { get; set; }
+            public IEnumerable<int> MyList { get; set; }
+        }
+
+        private class Target1
+        {
+            public int MyInt { get; set; }
+            public string MyString { get; set; }
+
+            public void SetMyInt(int myInt)
+            {
+                MyInt = myInt;
+            }
+
+            public IStatusGeneric SetMyString(string myString)
+            {
+                MyString = myString;
+                return new StatusGenericHandler {Message = "OK"};
+            }
+        }
+
+        [Fact]
+        public void TestBuildCallMethodNoReturn()
+        {
+            //SETUP 
+            var prop = typeof(Dto).GetProperty(nameof(Dto.MyInt));
+            var method = typeof(Target1).GetMethod(nameof(Target1.SetMyInt));
+            var dto = new Dto {MyInt = 123};
+            var target = new Target1();
+
+            //ATTEMPT
+            var action = method.CallMethodReturnVoid<Dto, Target1>(prop);
+            action.Invoke(dto, target);
+
+            //VERIFY
+            target.MyInt.ShouldEqual(123);
+        }
+
+        [Fact]
+        public void TestBuildCallMethodWithReturn()
+        {
+            //SETUP 
+            var prop = typeof(Dto).GetProperty(nameof(Dto.MyString));
+            var method = typeof(Target1).GetMethod(nameof(Target1.SetMyString));
+            var dto = new Dto { MyString = "Hello" };
+            var target = new Target1();
+
+            //ATTEMPT
+            var action = method.CallMethodReturnStatus<Dto, Target1>(prop);
+            var status = action.Invoke(dto, target);
+
+            //VERIFY
+            target.MyString.ShouldEqual("Hello");
+            status.Message.ShouldEqual("OK");
+        }
+
+    }
+}
