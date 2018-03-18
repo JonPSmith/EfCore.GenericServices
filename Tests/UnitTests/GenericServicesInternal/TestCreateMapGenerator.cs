7bda5119a681275084420e05909f8cce173ca418
Jon Smith <jon.smith@selectiveanalytics.com>
2018-03-18 16:25:35 +0000

    Added use of config to add to mapping

--- Tests/UnitTests/GenericServicesInternal/TestCreateMapGenerator.cs
+++ Tests/UnitTests/GenericServicesInternal/TestCreateMapGenerator.cs
@@ -9,7 +9,9 @@ using GenericServices;
 using Xunit;
 using GenericServices.Internal.Decoders;
 using GenericServices.Internal.MappingCode;
+using Tests.Configs;
 using Tests.Dtos;
+using Tests.Helpers;
 using TestSupport.EfHelpers;
 using Xunit.Extensions.AssertExtensions;
 
@@ -30,7 +32,7 @@ namespace Tests.UnitTests.GenericServicesInternal
         }
 
         [Fact]
-        public void TestAuthorBuildMappings()
+        public void TestAuthorReadMappings()
         {
             //SETUP
             var decodedDto = new DecodedDto(typeof(AuthorNameDto), _AuthorInfo);
@@ -43,12 +45,30 @@ namespace Tests.UnitTests.GenericServicesInternal
             //VERIFY
             var config = new MapperConfiguration(maps);
             var entity = new Author {AuthorId = 1, Name = "Author", Email = "me@nospam.com"};
-            //var dto = new AuthorNameDto { Name = "New Name" };
             var dto = config.CreateMapper().Map<AuthorNameDto>(entity);
             dto.Name.ShouldEqual("Author");
         }
 
-        
+        [Fact]
+        public void TestBookReadMappingsWithConfig()
+        {
+            //SETUP
+            var decodedDto = new DecodedDto(typeof(BookTitleAndCount), _bookInfo);
+            var maps = new MapperConfigurationExpression();
+
+            //ATTEMPT
+            var mapCreator = new CreateMapGenerator(decodedDto, _bookInfo, null, new BookTitleWithCountConfig());
+            mapCreator.Accessor.BuildReadMapping(maps);
+
+            //VERIFY
+            var config = new MapperConfiguration(maps);
+            var entity = DddEfTestData.CreateFourBooks().Last();
+            var dto = config.CreateMapper().Map<BookTitleAndCount>(entity);
+            dto.Title.ShouldEqual("Quantum Networking");
+            dto.ReviewsCount.ShouldEqual(2);
+        }
+
+
 
     }
 }
