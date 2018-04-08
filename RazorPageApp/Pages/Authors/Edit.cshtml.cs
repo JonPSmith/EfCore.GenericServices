e2440b6b4409f5251540cb8dfe525709a32b96ee
Jon Smith <jon.smith@selectiveanalytics.com>
2018-04-08 16:28:13 +0100

    Added author edit

--- RazorPageApp/Pages/Authors/Edit.cshtml.cs
+++ RazorPageApp/Pages/Authors/Edit.cshtml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer.EfClasses;
 using GenericServices;
+using GenericServices.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceLayer.HomeController.Dtos;
 
 namespace RazorPageApp.Pages.Authors
 {
@@ -17,10 +20,31 @@ namespace RazorPageApp.Pages.Authors
             _service = service;
         }
 
+        [BindProperty]
+        public Author Data { get; set; }
 
-        public void OnGet()
+        public void OnGet(int id)
         {
+            Data = _service.ReadSingle<Author>(id);
+            if (!_service.IsValid)
+            {
+                _service.CopyErrorsToModelState(ModelState, Data);
+            }
+        }
 
+        public IActionResult OnPost(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _service.UpdateAndSave(Data);
+            if (_service.IsValid)
+                return RedirectToPage("Index", new { message = _service.Message });
+
+            //Error state
+            _service.CopyErrorsToModelState(ModelState, Data, "Data");
+            return Page();
         }
     }
 }
