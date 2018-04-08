656786afece027474a488086537ef78018df120c
Jon Smith <jon.smith@selectiveanalytics.com>
2018-04-08 11:50:47 +0100

    Had to change folder to "authors" so I could use Author class

---
+++ RazorPageApp/Pages/Authors/Edit.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GenericServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageApp.Pages.Authors
+{
+    public class EditModel : PageModel
+    {
+        private readonly ICrudServices _service;
+
+        public EditModel(ICrudServices service)
+        {
+            _service = service;
+        }
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
