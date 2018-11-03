f1345c1378bf374574c5e4b6154696f782b14acd
Jon Smith <jon.smith@selectiveanalytics.com>
2018-11-03 09:46:39 +0000

    Added link to AutoMapper Reverse mapping docs

--- Tests/Configs/InContactAddressConfig.cs
+++ Tests/Configs/InContactAddressConfig.cs
@@ -11,6 +11,8 @@ namespace Tests.Configs
 {
     public class InContactAddressConfig : PerDtoConfig<InContactAddressDto, ContactAddress>
     {
+        //See http://docs.automapper.org/en/stable/Reverse-Mapping-and-Unflattening.html#customizing-reverse-mapping on reverse mapping
+        //my test showed that I didn't need the .ReverseMap() - that's most likely because I use AutoMapper's Map, rather that MapFrom
         public override Action<IMappingExpression<InContactAddressDto, ContactAddress>> AlterSaveMapping
         {
             get
