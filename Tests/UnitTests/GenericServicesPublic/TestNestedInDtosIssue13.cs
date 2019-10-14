331299a4f9afd19dad2824cd731e1a95c963d8d6
Florian Raffl <florian.raffl@ntw-software.com>
2019-10-14 11:26:37 +0200

    added test for linq query with nested DTO where clause

--- Tests/UnitTests/GenericServicesPublic/TestNestedInDtosIssue13.cs
+++ Tests/UnitTests/GenericServicesPublic/TestNestedInDtosIssue13.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2018 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
 // Licensed under MIT licence. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Linq;
 using GenericServices.PublicButHidden;
 using GenericServices.Setup;
@@ -45,9 +46,41 @@ namespace Tests.UnitTests.GenericServicesPublic
                 contact.Name.ShouldEqual("test");
                 contact.Address.Address1.ShouldEqual("some street");
             }
-        }
+		}
 
-        
+		[Fact]
+		public void TestInDtosNestedOk2()
+		{
+			//SETUP
+			var options = SqliteInMemory.CreateOptions<TestDbContext>();
+			using (var context = new TestDbContext(options))
+			{
+				context.Database.EnsureCreated();
 
-    }
+				var utData = context.SetupSingleDtoAndEntities<InContactAddressDto>();
+				utData.AddSingleDto<InAddressDto>();
+				var service = new CrudServices(context, utData.ConfigAndMapper);
+
+				//ATTEMPT
+				var dto = new InContactAddressDto
+				{
+					Name = "test",
+					Addess = new InAddressDto
+					{
+						Address1 = "some street"
+					}
+				};
+				service.CreateAndSave(dto);
+
+				//VERIFY
+				service.IsValid.ShouldBeTrue(service.GetAllErrors());
+				var contact = context.ContactAddresses.SingleOrDefault();
+				contact.Name.ShouldEqual("test");
+
+				IList<InContactAddressDto> resp = service.ReadManyNoTracked<InContactAddressDto>().Where(x => x.Addess.Address1 == "some street").ToList();
+
+				contact.Address.Address1.ShouldEqual("some street");
+			}
+		}
+	}
 }
