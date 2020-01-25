ce147a64d13bf10b6cac7a57a85312b66e7ecdff
Jon Smith <jon.smith@selectiveanalytics.com>
2020-01-25 08:48:02 +0000

    Updated tests

--- Tests/UnitTests/GenericServicesPublic/TestUpdateWithIncludeThenAttribute.cs
+++ Tests/UnitTests/GenericServicesPublic/TestUpdateWithIncludeThenAttribute.cs
@@ -3,17 +3,13 @@
 
 using System;
 using System.Linq;
-using DataLayer.EfClasses;
+using System.Threading.Tasks;
 using DataLayer.EfCode;
-using GenericServices;
-using GenericServices.Configuration;
-using GenericServices.Internal.Decoders;
 using GenericServices.PublicButHidden;
 using GenericServices.Setup;
 using Microsoft.EntityFrameworkCore;
 using Tests.Dtos;
 using Tests.Helpers;
-using Tests.UnitTests.GenericServicesInternal;
 using TestSupport.EfHelpers;
 using Xunit;
 using Xunit.Extensions.AssertExtensions;
@@ -22,30 +18,33 @@ namespace Tests.UnitTests.GenericServicesPublic
 {
     public class TestUpdateWithIncludeThenAttribute
     {
-
-
         [Fact]
         public void TestCallAddReviewWithIncludeWithIncludedReviews()
         {
             //SETUP
+            int bookId;
             var options = SqliteInMemory.CreateOptions<EfCoreContext>();
             using (var context = new EfCoreContext(options))
             {
                 context.Database.EnsureCreated();
                 var bookSeeded = context.SeedDatabaseFourBooks();
+                bookId = bookSeeded.Last().BookId;
+            }
 
+            using (var context = new EfCoreContext(options))
+            {
                 var utData = context.SetupSingleDtoAndEntities<AddReviewWithIncludeDto>();
                 var service = new CrudServices(context, utData.ConfigAndMapper);
 
                 //ATTEMPT
                 var dto = new AddReviewWithIncludeDto
-                    {BookId = bookSeeded.Last().BookId, Comment = "bad", NumStars = 1, VoterName = "user"};
+                    {BookId = bookId, Comment = "bad", NumStars = 1, VoterName = "user"};
                 service.UpdateAndSave(dto);
 
                 //VERIFY
                 service.IsValid.ShouldBeTrue(service.GetAllErrors());
                 service.Message.ShouldEqual("Successfully updated the Book");
-                var entity = context.Books.Include(x => x.Reviews).Single(x => x.BookId == bookSeeded.Last().BookId);
+                var entity = context.Books.Include(x => x.Reviews).Single(x => x.BookId == bookId);
                 entity.Reviews.Count().ShouldEqual(3);
                 entity.Reviews.Single(x => x.Comment == "bad").NumStars.ShouldEqual(1);
             }
@@ -55,20 +54,27 @@ namespace Tests.UnitTests.GenericServicesPublic
         public void TestCallUpdateBookWithExistingAuthorWithTwoIncludes()
         {
             //SETUP
+            int bookId;
+            int authorId;
             var options = SqliteInMemory.CreateOptions<EfCoreContext>();
             using (var context = new EfCoreContext(options))
             {
                 context.Database.EnsureCreated();
                 var bookSeeded = context.SeedDatabaseFourBooks();
+                bookId = bookSeeded.First().BookId;
+                authorId = bookSeeded.Last().AuthorsLink.First().AuthorId;
+            }
 
+            using (var context = new EfCoreContext(options))
+            {
                 var utData = context.SetupSingleDtoAndEntities<UpdateBookWithAuthorUsingIncludeDto>();
                 var service = new CrudServices(context, utData.ConfigAndMapper);
 
                 //ATTEMPT
                 var dto = new UpdateBookWithAuthorUsingIncludeDto
                 {
-                    BookId = bookSeeded.First().BookId, 
-                    AddThisAuthor = bookSeeded.Last().AuthorsLink.First().Author,
+                    BookId = bookId,
+                    AddThisAuthor = context.Authors.SingleOrDefault(x => x.AuthorId == authorId),
                     Order = 2
                 };
                 service.UpdateAndSave(dto);
@@ -77,10 +83,85 @@ namespace Tests.UnitTests.GenericServicesPublic
                 service.IsValid.ShouldBeTrue(service.GetAllErrors());
                 service.Message.ShouldEqual("Successfully updated the Book");
                 var bookAuthorsName = context.Books
-                    .Where(x => x.BookId == bookSeeded.First().BookId)
+                    .Where(x => x.BookId == bookId)
                     .SelectMany(x => x.AuthorsLink.Select(y => y.Author.Name))
                     .ToArray();
-                bookAuthorsName.ShouldEqual(new String[] { "Martin Fowler", "Future Person" });
+                bookAuthorsName.ShouldEqual(new String[] {"Martin Fowler", "Future Person"});
+            }
+        }
+
+        //-----------------------------------------------
+        //async versions
+
+        [Fact]
+        public async Task TestCallAddReviewWithIncludeWithIncludedReviewsAsync()
+        {
+            //SETUP
+            int bookId;
+            var options = SqliteInMemory.CreateOptions<EfCoreContext>();
+            using (var context = new EfCoreContext(options))
+            {
+                context.Database.EnsureCreated();
+                var bookSeeded = context.SeedDatabaseFourBooks();
+                bookId = bookSeeded.Last().BookId;
+            }
+
+            using (var context = new EfCoreContext(options))
+            {
+                var utData = context.SetupSingleDtoAndEntities<AddReviewWithIncludeDto>();
+                var service = new CrudServicesAsync(context, utData.ConfigAndMapper);
+
+                //ATTEMPT
+                var dto = new AddReviewWithIncludeDto
+                    {BookId = bookId, Comment = "bad", NumStars = 1, VoterName = "user"};
+                await service.UpdateAndSaveAsync(dto);
+
+                //VERIFY
+                service.IsValid.ShouldBeTrue(service.GetAllErrors());
+                service.Message.ShouldEqual("Successfully updated the Book");
+                var entity = context.Books.Include(x => x.Reviews).Single(x => x.BookId == bookId);
+                entity.Reviews.Count().ShouldEqual(3);
+                entity.Reviews.Single(x => x.Comment == "bad").NumStars.ShouldEqual(1);
+            }
+        }
+
+        [Fact]
+        public async Task TestCallUpdateBookWithExistingAuthorWithTwoIncludesAsync()
+        {
+            //SETUP
+            int bookId;
+            int authorId;
+            var options = SqliteInMemory.CreateOptions<EfCoreContext>();
+            using (var context = new EfCoreContext(options))
+            {
+                context.Database.EnsureCreated();
+                var bookSeeded = context.SeedDatabaseFourBooks();
+                bookId = bookSeeded.First().BookId;
+                authorId = bookSeeded.Last().AuthorsLink.First().AuthorId;
+            }
+
+            using (var context = new EfCoreContext(options))
+            {
+                var utData = context.SetupSingleDtoAndEntities<UpdateBookWithAuthorUsingIncludeDto>();
+                var service = new CrudServicesAsync(context, utData.ConfigAndMapper);
+
+                //ATTEMPT
+                var dto = new UpdateBookWithAuthorUsingIncludeDto
+                {
+                    BookId = bookId,
+                    AddThisAuthor = context.Authors.SingleOrDefault(x => x.AuthorId == authorId),
+                    Order = 2
+                };
+                await service.UpdateAndSaveAsync(dto);
+
+                //VERIFY
+                service.IsValid.ShouldBeTrue(service.GetAllErrors());
+                service.Message.ShouldEqual("Successfully updated the Book");
+                var bookAuthorsName = context.Books
+                    .Where(x => x.BookId == bookId)
+                    .SelectMany(x => x.AuthorsLink.Select(y => y.Author.Name))
+                    .ToArray();
+                bookAuthorsName.ShouldEqual(new String[] {"Martin Fowler", "Future Person"});
             }
         }
     }
