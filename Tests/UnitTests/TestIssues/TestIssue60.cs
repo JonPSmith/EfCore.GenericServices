225031bdd97aa174ddb325c9a5e176dbbcb2a3c0
Jon P Smith <jon.smith@selectiveanalytics.com>
2022-02-01 14:27:00 +0000

    Created tests for issue #60

---
+++ Tests/UnitTests/TestIssues/TestIssue60.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) 2022 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System.ComponentModel.DataAnnotations;
+using GenericServices.PublicButHidden;
+using GenericServices.Setup;
+using Microsoft.EntityFrameworkCore;
+using TestSupport.EfHelpers;
+using Xunit;
+
+namespace Tests.UnitTests.TestIssues
+{
+    public class TestIssue60
+    {
+        public class entity
+        {
+            [Key]
+            public long Id { get; set; }
+            public Reference Object { get; set; }
+            public Reference Object2 { get; set; }
+        }
+
+        [Owned]
+        public class Reference
+        {
+            [StringLength(50)]
+            public string Type { get; set; }
+        }
+
+        public class Context : DbContext
+        {
+            public Context(DbContextOptions<Context> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<TestIssue60.entity> Entities { get; set; }
+        }
+
+        [Fact]
+        public void Test60()
+        {
+            var options = SqliteInMemory.CreateOptions<Context>();
+            using (var context = new Context(options))
+            {
+                context.Database.EnsureCreated();
+                var utData = context.SetupEntitiesDirect();
+
+                var service = new CrudServices(context, utData.ConfigAndMapper);
+            }
+        }
+    }
+}
